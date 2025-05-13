35b4086fd5bccc812a9f2f8f6f821b80c511b2d9
AgrimNarang <148768466+AgrimNarang@users.noreply.github.com>
2025-05-13 12:35:41 +0530

    modified:   package.json 	modified:   src/Routes.jsx

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // Import page components
 import HomePage from './pages/Home';
 import Events from './pages/events';
+import Dashboard from './pages/dashboard';
 
 const AppRoutes = () => {
   return (
@@ -11,6 +12,7 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/events" element={<Events />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
